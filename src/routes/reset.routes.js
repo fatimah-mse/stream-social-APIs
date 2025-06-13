ed03d7ccbb3112a99dd557a1aa2d09e8028c8b06
fatimah-mse <fatimahmse83@gmail.com>
2025-06-13 16:13:18 +0300

    edit-gitignore

--- src/routes/reset.routes.js
+++ src/routes/reset.routes.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const {auth} = require('../middlewares/auth.middleware')
-const resetController = new (require('../controllers/reset.controller'))
+const resetController = (require('../controllers/reset.controller'))
 
 router.put('/', [auth], resetController.resetAccount)
 
