0f0b0e5d10ee00fa56c538bc7a4cffc048e109af
fatimah-mse <fatimahmse83@gmail.com>
2025-06-14 13:12:36 +0300

    add-cors

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const ResetRoutes = require('./routes/reset.routes')
 
 app.use(express.json())
 app.use(morgan("dev"))
-// const cors = require("cors")
+const cors = require("cors")
 
 app.use(cors({
     origin: "http://localhost:5173/stream-social", 
