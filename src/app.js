b7fefbd9330ca23f903562fea6cde44b9d1a45af
fatimah-mse <fatimahmse83@gmail.com>
2025-06-15 02:11:43 +0300

    add-cors

--- src/app.js
+++ src/app.js
@@ -12,8 +12,19 @@ app.use(express.json())
 app.use(morgan("dev"))
 const cors = require("cors")
 
+const allowedOrigins = [
+    "http://localhost:5173",
+    "https://fatimah-mse.github.io"
+]
+
 app.use(cors({
-    origin: ["http://localhost:5173", "https://fatimah-mse.github.io/stream-social/"],
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true)
+        } else {
+            callback(new Error("Not allowed by CORS"))
+        }
+    },
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true
